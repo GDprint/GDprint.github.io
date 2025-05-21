2fc3f592890357fc02dddfa38ed74428cb6adae5
GDprints <120603682+GDprint@users.noreply.github.com>
2025-05-21 21:54:24 +0300

    Update order_am.js

--- assets/js/order_am.js
+++ assets/js/order_am.js
@@ -54,8 +54,7 @@ const orderData = {
 		images: [
         "https://github.com/GDprint/GDprint.github.io/blob/Master/assets/img/bandicam%202025-04-12%2019-49-12-564.jpg?raw=true"
             ]
-	},
-	"LPT573886": {
+	}, "LPT-573886": {
 		title: "Պատվեր #LPT-573886 - Լայնաֆորմատ տպագրություն",
 		printingMaterial: "Banner-ի վրա տպագրություն",
 		orderStage: "Պատվերը պատրաստ է դիտարկման",
@@ -68,8 +67,10 @@ const orderData = {
 		images: [
         "https://github.com/GDprint/GDprint.github.io/blob/Master/assets/img/portfolio/LPT-573886.jpg?raw=true"
             ]
-};
+	}
 
+};
+// 
 function searchOrder() {
 	const orderNumber = document.getElementById("orderNumber").value.trim();
 	const message = document.getElementById("message");
