b023286095a4ea6d40c09521b24d66df3547d80e
GDprints <120603682+GDprint@users.noreply.github.com>
2026-02-07 15:37:33 +0400

    Update order_am.js

--- assets/js/order_am.js
+++ assets/js/order_am.js
@@ -111,7 +111,7 @@ const orderData = {
 			phone: "+37491499001"
 		},
 		description: "",
-			images: ["https://github.com/GDprint/GDprint.github.io/blob/Master/assets/img/portfolio/LTP-822593.jpg?raw=true"]
+			images: ["https://github.com/GDprint/GDprint.github.io/blob/Master/assets/img/portfolio/HH.jpg?raw=true"]
 	}
 	
 
@@ -405,5 +405,6 @@ function displayStats() {
         </div>
     `;
 }
+
 
 
